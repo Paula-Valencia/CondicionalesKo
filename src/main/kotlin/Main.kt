cc22a2785bb1dc51cf50bd13f75bd537f389d76e
Paula-Valencia <pvalenciamona@gmail.com>
2024-06-13 11:34:53 -0500

    Cambios

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,74 @@
 package org.example
 
 fun main() {
-    println("Hello World!")
+   /* //CONDICIONALES 1PUNTO
+   val trafficLightColor = "Black"
+
+    if (trafficLightColor == "Red") {
+        println("Stop")
+    } else if (trafficLightColor == "Yellow") {
+        println("Slow")
+    }
+    else if (trafficLightColor == "Green"){
+        println("Go")
+    }
+    else {
+        println("Invalid traffic-light color")
+    }  */
+
+    /*
+    //SENTENCIA 1 2PUNTO
+    val trafficLightColor = "Yellow"
+    when (trafficLightColor) {
+        "Red" -> println("Shop")
+        "Yellow" -> println("Slow")
+        "Green" -> println("Go")
+        else -> println("Invalid traffic-light color")
+    }
+     */
+    /*
+    //SENTENCIA 2
+    val x: Any = 20
+
+    when (x) {
+        2, 3, 5, 7 -> println("x is a prime number between 1 and 10.")
+        in 1..10 -> println("x is a number between 1 and 10, but not a prime number.")
+        is Int -> println("x is an integer number, but not between 1 and 10.")
+        else -> println("x isn't an integer number.")
+    }
+     */
+
+    /*
+    //CONFICIONAL ADICIONAL
+    val trafficLightColor = "Amber"
+
+    when (trafficLightColor) {
+        "Red" -> println("Stop")
+        "Yellow", "Amber" -> println("Slow")
+        "Green" -> println("Go")
+        else -> println("Invalid traffic-light color")
+    }
+     */
+
+    //EXPRESIONES 3PUNTO
+    /* //1
+    val trafficLightColor = "Black"
+
+    val message =
+        if (trafficLightColor == "Red") "Stop"
+        else if (trafficLightColor == "Yellow") "Slow"
+        else if (trafficLightColor == "Green") "Go"
+        else "Invalid traffic-light color"
+        println(message)
+     */
+    //2
+    val trafficLightColor = "Amber"
+
+    val message = when(trafficLightColor) {
+        "Red" -> "Stop"
+        "Yellow", "Amber" -> "Proceed with caution."
+        "Green" -> "Go"
+        else -> "Invalid traffic-light color"
+    }
+    println(message)
 }
